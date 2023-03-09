2d3d06a808db5fc6971ff26e01ceb7c5a387f45c
Dante PC CERN <d.niewenhuis@hotmail.com>
2023-03-09 10:40:48 +0100

    test change

--- Python_files/test.py
+++ Python_files/test.py
@@ -14,29 +14,28 @@ main_folder = "../"
 tree_name = "test_tree"
 file_name = f"{main_folder}data/Higgs_data_full.root"
 
-tree_name = "sig_tree"
-file_name = f"{main_folder}data/vectorData.root"
+# tree_name = "sig_tree"
+# file_name = f"{main_folder}data/vectorData.root"
 
-batch_rows = 1
-chunk_rows = 1
+batch_rows = 1024
+chunk_rows = 1_000_000
 
 columns = ['jet1_btag', 'jet1_eta', 'Type']
 columns = []
 
-vec_sizes = [3]
 
 ds_train, ds_validation = GetTFDatasets(file_name, tree_name, chunk_rows,
-                           batch_rows, columns, vec_sizes, validation_split=0.3, max_chunks=1)
+                           batch_rows, columns, validation_split=0.3, target = "Type")
 
 # gen_train, gen_validation = GetGenerators(file_name, tree_name, chunk_rows,
 #                            batch_rows, target="Type", validation_split=0.3, max_chunks=2)
 
 
-for item in ds_train:
-    print(f"train: {type(item)}, {item}")
+# for item in ds_train:
+#     print(f"train: {type(item)}, {item}")
     
-for item in ds_validation:
-    print(f"validation: {type(item)}, {item}")
+# for item in ds_validation:
+#     print(f"validation: {type(item)}, {item}")
     
 # num_columns = len(train_generator.columns)
 
@@ -44,18 +43,18 @@ for item in ds_validation:
 # ## AI example
 # ###################################################################################################
 
-# model = tf.keras.Sequential([
-#     tf.keras.layers.Dense(300, activation=tf.nn.tanh, input_shape=(28,)),  # input shape required
-#     tf.keras.layers.Dense(300, activation=tf.nn.tanh),
-#     tf.keras.layers.Dense(300, activation=tf.nn.tanh),
-#     tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
-# ])
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(300, activation=tf.nn.tanh, input_shape=(28,)),  # input shape required
+    tf.keras.layers.Dense(300, activation=tf.nn.tanh),
+    tf.keras.layers.Dense(300, activation=tf.nn.tanh),
+    tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
 
-# loss_fn = tf.keras.losses.BinaryCrossentropy()
+loss_fn = tf.keras.losses.BinaryCrossentropy()
 
-# model.compile(optimizer='adam',
-#               loss=loss_fn,
-#               metrics=['accuracy'])
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
 
 
-# model.fit(ds_train, validation_data=ds_validation, epochs=3)
+model.fit(ds_train, validation_data=ds_validation, epochs=3)
