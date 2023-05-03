a89424d61e719d55ed597ee43fc19835fabc263f
d.niewenhuis@hotmail.com <d.niewenhuis@hotmail.com>
2023-05-03 18:03:57 +0200

    added svg images

--- benchmarks/memory_test/analysis.py
+++ benchmarks/memory_test/analysis.py
@@ -5,8 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import os
-
-# %%
+axis_fontsize = 18
 
 df = pd.read_csv("results/memory.csv")
 chunk_size = 1_000_000
@@ -21,21 +20,29 @@ def get_size(num):
 df["size"] = df.apply(lambda row: get_size(row["num"]), axis=1)
 # %%
 
+line_width = 2.5
+
 df = df[df["num"] < 13]
 
 df_ROOT = df[df["method"] == "ROOT"]
 df_uproot = df[df["method"] == "uproot"]
 
-plt.plot(df_uproot["size"], df_uproot["maxresident"], label="uproot")
+plt.plot(df_uproot["size"], df_uproot["maxresident"], label="uproot-tensorflow", linewidth=line_width)
 
+labels = ["RBG 10K", "RBG 100K", "RBG 1M", "RBG 2M", "RBG 10M", "RBG 20M"]
+
+i = 0
 for cs, rows in df_ROOT.groupby("chunksize"):
-    label = f'ROOT {cs:.0f}'
-    # while len(label) < 12:
-    #     label = label[:4] + " " + label[4:]
-    plt.plot(rows["size"], rows["maxresident"], label=label)
+    
+    label = labels[i]
+    i += 1
+    plt.plot(rows["size"], rows["maxresident"], label=label, linewidth=line_width)
 
-plt.xlabel("file size (b)")
-plt.ylabel("memory usage (b)")
+plt.xlabel("file size (b)", fontsize=axis_fontsize)
+plt.ylabel("memory usage (b)", fontsize=axis_fontsize)
+
+plt.tick_params(axis='both', which='major', labelsize=13)
+# plt.tick_params(axis='both', which='minor', labelsize=10)
 
 left_lim, right_lim = plt.xlim()
 bot_lim, top_lim = plt.ylim()
@@ -43,10 +50,10 @@ bot_lim, top_lim = plt.ylim()
 plt.xlim(0,right_lim)
 plt.ylim(0,top_lim)
 
-plt.legend()
+plt.legend(fontsize=12)
 plt.tight_layout()
 
-plt.savefig("images/scaling_memory.png")
+plt.savefig("images/scaling_memory.svg", format="svg")
 
 # %%
 
@@ -83,22 +90,24 @@ for num in range(10,14):
 
 file_sizes = [get_size(i) for i in range(1,14)]
 
-plt.plot(file_sizes, load_time_uproot, label="uproot")
+plt.plot(file_sizes, load_time_uproot, label="uproot-tensorflow")
 
 for cs, load_times in zip(chunk_sizes, load_times_ROOT):
-    plt.plot(file_sizes, load_times, label=f"ROOT {cs:.0f}")
+    plt.plot(file_sizes, load_times, label=f"RGB {cs:.0f}")
 
 
 # plt.plot([get_size(i) for i in range(5,14)], load_10mil, label=f"ROOT {10_000_000:.0f}")
 # plt.plot([get_size(i) for i in range(10,14)], load_20mil, label=f"ROOT {20_000_000:.0f}")
 
 
-plt.ylabel("loading time (s)")
-plt.xlabel("file size")
+plt.tick_params(axis='both', which='major', labelsize=13)
+
+plt.ylabel("loading time (s)", fontsize=axis_fontsize)
+plt.xlabel("file size (byte)", fontsize=axis_fontsize)
 
 plt.legend()
 plt.tight_layout()
-plt.savefig("images/scaling_loading_time.png")
+plt.savefig("images/scaling_loading_time.svg", format="svg")
 
 # %%
 
@@ -108,15 +117,17 @@ plt.savefig("images/scaling_loading_time.png")
 df_uproot = pd.read_csv(f"results/Tensorflow_uproot_5_train.csv", header=None, names=["time"])
 df_ROOT = pd.read_csv(f"results/Tensorflow_ROOT_5_1000000_train.csv", header=None, names=["time"])
 
-plt.hist(df_uproot[(df_uproot["time"] < 0.01) & (df_uproot["time"] > 0)]["time"], bins=100, label="uproot", alpha=.75)
-plt.hist(df_ROOT[(df_ROOT["time"] < 0.01) & (df_ROOT["time"] > 0)]["time"], bins=100, label="ROOT", alpha=.75)
+plt.hist(df_uproot[(df_uproot["time"] < 0.01) & (df_uproot["time"] > 0)]["time"]*1000, bins=100, label="TensorFlow", alpha=.75)
+plt.hist(df_ROOT[(df_ROOT["time"] < 0.01) & (df_ROOT["time"] > 0)]["time"]*1000, bins=100, label="RBatchGenerator", alpha=.75)
 
-plt.xlabel("batch loading time (s)")
+plt.xlabel("batch processing time (ms)", fontsize=axis_fontsize)
+plt.ylabel("count", fontsize=axis_fontsize)
 
-plt.legend()
+plt.tick_params(axis='both', which='major', labelsize=12)
+plt.legend(fontsize=15)
 
 plt.tight_layout()
-plt.savefig("images/loading_hist.png")
+plt.savefig("images/loading_hist.svg", format="svg")
 
 # %%
 
@@ -145,7 +156,7 @@ plt.plot(range(len(cum_uproot)), cum_uproot, label="uproot", alpha=0.75)
 plt.legend()
 
 plt.tight_layout()
-plt.savefig("images/ROOT_vs_uproot.png")
+plt.savefig("images/ROOT_vs_uproot.svg", format="svg")
 
 
 # %%
@@ -163,16 +174,16 @@ df_parallel["time_norm"] = df_parallel["time"] - (0.1 * df_parallel.index)
 
 # %%
 
-plt.plot(df_single["time_norm"], label="single")
-plt.plot(df_parallel["time_norm"], label="parallel")
+plt.plot(df_single["time_norm"], label="single", linewidth=2.5)
+plt.plot(df_parallel["time_norm"], label="parallel", linewidth=2.5)
 
-plt.xlabel("batch number")
-plt.ylabel("time (s)")
+plt.xlabel("batch number", fontsize=axis_fontsize)
+plt.ylabel("time (s)", fontsize=axis_fontsize)
 
-plt.legend()
+plt.legend(fontsize=18)  
 
 plt.tight_layout()
-plt.savefig(f"images/single_vs_parallel.png")
+plt.savefig(f"images/single_vs_parallel.svg", format="svg")
 # %%
 
 df_single["time_norm"]
---
+++ benchmarks/memory_test/sample_code_powerpoint.py
@@ -0,0 +1,29 @@
+from ROOT.TMVA.Experimental import GetTFDatasets, GetBatchGenerators, GetPyTorchDataLoaders 
+
+main_folder = "/home/danteniewenhuis/Documents/CERN/TMVA-batching"
+
+tree_name = "test_tree"
+file_name = f"{main_folder}/data/file_sizes_bench/{num}.root"
+target = "Type"
+
+1
+batch_rows = 1024
+chunk_rows = 100
+
+
+
+
+# Returns a generator that returns NumPy arrays
+gen_train, gen_validation = GetBatchGenerators(file_name, tree_name, chunk_rows,
+                            batch_rows, target="Type", validation_split=0.3)
+
+# Returns a TensorFlow Dataset
+ds_train, ds_validation = GetTFDatasets(
+                            file_name, tree_name, 
+                            chunk_rows,batch_rows, 
+                            target="Type", 
+                            validation_split=0.3)
+
+# Returns a generator that returns PyTorch Tensors
+gen_train, gen_validation = GetPyTorchDataLoaders(file_name, tree_name, chunk_rows,
+                            batch_rows, target="Type", validation_split=0.3)
